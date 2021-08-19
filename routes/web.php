2ad849826c6fc0c66dcfa32021e2f5f954e2ecb5
AravindaMuthugala <aravinda240@gmail.com>
2021-08-19 23:17:02 +0530

    route change

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
+Route::get('/', 'DashboardController@index');
+
+Route::group(['middleware' => 'auth:web'], function(){
+
+
 //Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/profile', 'ProfileController@index')->name('profile');
-Route::put('/profile', 'ProfileController@update')->name('profile.update');
+    Route::get('/profile', 'ProfileController@index')->name('profile');
+    Route::put('/profile', 'ProfileController@update')->name('profile.update');
 
 //Route::get('/otp_key', 'OtpController@index')->name('otpApiKey');
 
@@ -31,24 +36,24 @@ Route::put('/profile', 'ProfileController@update')->name('profile.update');
 //    Route::get('home', 'HomeController@index')->name('home');
 //});
 
-Route::prefix('/')->group(function () {
-    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
-    Route::get('otp_key', 'OtpController@index')->name('otpApiKey');
-    Route::post('generate_common_key', 'OtpController@generateKey')->name('generateKey');
-});
+    Route::prefix('/')->group(function () {
+        Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+        Route::get('otp_key', 'OtpController@index')->name('otpApiKey');
+        Route::post('generate_common_key', 'OtpController@generateKey')->name('generateKey');
+    });
 
 
 
-Route::prefix('/report_management')->group(function () {
-    Route::get('/', 'ReportController@viewCommonReport')->name('viewCommonReport');
-    Route::get('/daily/{appId?}/{fDate?}/{tDate?}', 'ReportController@viewDailyReport')->name('viewDailyReport');
-    Route::get('/filterDaily/{appId?}/{fDate?}/{tDate?}', 'ReportController@filterDailyReport')->name('filterDailyReport');
-});
+    Route::prefix('/report_management')->group(function () {
+        Route::get('/', 'ReportController@viewCommonReport')->name('viewCommonReport');
+        Route::get('/daily/{appId?}/{fDate?}/{tDate?}', 'ReportController@viewDailyReport')->name('viewDailyReport');
+        Route::get('/filterDaily/{appId?}/{fDate?}/{tDate?}', 'ReportController@filterDailyReport')->name('filterDailyReport');
+    });
 
-Route::prefix('/category_management')->group(function () {
-    Route::get('/{id?}', 'CategoryController@viewCategories')->name('viewCategories');
-    Route::get('/delete/{id}', 'CategoryController@deleteCategory');
-    Route::post('/add_category', 'CategoryController@addCategory')->name('addCategory');
+    Route::prefix('/category_management')->group(function () {
+        Route::get('/{id?}', 'CategoryController@viewCategories')->name('viewCategories');
+        Route::get('/delete/{id}', 'CategoryController@deleteCategory');
+        Route::post('/add_category', 'CategoryController@addCategory')->name('addCategory');
 
 
 //    Route::post('/add_content', 'CategoryController@addContent')->name('addContent');
@@ -56,13 +61,13 @@ Route::prefix('/category_management')->group(function () {
 //    Route::get('/filter_content/{appId?}', 'CategoryController@filterContent')->name('filterContent');
 //    Route::get('/get_category/{catId?}', 'CategoryController@getCategory')->name('getCategory');
 //    Route::get('/contents', 'CategoryController@viewContents');
-});
+    });
 
-Route::prefix('/content_management')->group(function () {
-    Route::get('/{id?}', 'ContentController@view')->name('viewContents');
-    Route::get('/delete/{id}', 'ContentController@delete');
-    Route::post('/add', 'ContentController@add')->name('addContent');
-});
+    Route::prefix('/content_management')->group(function () {
+        Route::get('/{id?}', 'ContentController@view')->name('viewContents');
+        Route::get('/delete/{id}', 'ContentController@delete');
+        Route::post('/add', 'ContentController@add')->name('addContent');
+    });
 //Route::prefix('/category_management')->group(function () {
 //    Route::get('/', 'CategoryController@viewCategories')->name('viewCategories');
 //    Route::post('/add_category', 'CategoryController@addCategory')->name('addCategory');
@@ -73,9 +78,9 @@ Route::prefix('/content_management')->group(function () {
 //    Route::get('/contents', 'CategoryController@viewContents')->name('viewContents');
 //});
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
 
 //Route::get('/new_application', function () {
 //    return view('newApplication');
@@ -85,23 +90,22 @@ Route::get('/about', function () {
 //    return view('myApplications');
 //})->name('myApplications');
 
-Route::get('/categories', function () {
-    return view('categories');
-})->name('categories');
+    Route::get('/categories', function () {
+        return view('categories');
+    })->name('categories');
 
-Route::get('/content', function () {
-    return view('content');
-})->name('content');
+    Route::get('/content', function () {
+        return view('content');
+    })->name('content');
 
-Route::get('/application_report', function () {
-    return view('applicationReport');
-})->name('applicationReport');
+    Route::get('/application_report', function () {
+        return view('applicationReport');
+    })->name('applicationReport');
 
-Route::get('/daily_report', function () {
-    return view('dailyReport');
-})->name('dailyReport');
+    Route::get('/daily_report', function () {
+        return view('dailyReport');
+    })->name('dailyReport');
 
-Route::group(['middleware' => 'auth:web'], function(){
     Route::prefix('/app_management')->group(function () {
         Route::get('/', 'AppController@index')->name('viewApps');
         Route::get('/create', 'AppController@createApp')->name('createApp');
